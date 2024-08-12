3808e0276a29ff64de24fb140d56acc88baa6780
MyCoding-Hub <breacher.darkworld@gmail.com>
2024-08-12 18:57:11 +0530

    Using Spread operator to pass objects as props

--- amazon-seller/src/index.js
+++ amazon-seller/src/index.js
@@ -2,38 +2,41 @@ import React from 'react'
 import ReactDOM from "react-dom/client"
 import './index.css' ;
 
-const books = [{author:'Robert.T Kiyosaki' ,
-  bookTitle:'Rich Dad and Poor Dad' ,
+const books = [{
+  id:1,
+  author:'Robert.T Kiyosaki' ,
+  title:'Rich Dad and Poor Dad' ,
   img:'https://images-eu.ssl-images-amazon.com/images/I/81BE7eeKzAL._AC_UL900_SR900,600_.jpg'
 }, {
+  id:2,
   author :'The Housemaid', 
-  bookTitle:'Freida McFadden', 
+  title:'Freida McFadden', 
   img: 'https://images-na.ssl-images-amazon.com/images/I/81AHTyq2wVL._AC_UL900_SR900,600_.jpg' 
 }, {
+  id:3,
   author:'James Clear', 
-  bookTitle:'Atomic Habits', 
+  title:'Atomic Habits', 
   img:'https://images-na.ssl-images-amazon.com/images/I/81YkqyaFVEL._AC_UL900_SR900,600_.jpg'
 } ]
 
 const Booklist = () => {
   return(
   <section className='bookList'>
-    {books.map((books) => { const {author, bookTitle, img} = books
-  return(<Book author = {author} title = {bookTitle} img = {img}/>) })}
+    {books.map((books) => { 
+  return(<Book {...books} key= {books.id}/> ) })}
   </section>)}
 
 
 const Book = (props) => {
 
 const {author, title, img} = props
-
+console.log(props)
 const styleHeading = {color:'#617d98', fontSize:'0.75rem', marginTop:'0.5rem'}
     return(
         <article className='book'>
           <img src={img} alt={title}/>
           <h4>{title}</h4>
-          <h4 style={styleHeading}> {author}</h4>
-          
+          <h4 style={styleHeading}> {author}</h4>  
         </article>
     )
 }
