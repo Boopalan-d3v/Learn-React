aaee2a48ab948f2d31477ffdded2eb9e6f0397a7
MyCoding-Hub <breacher.darkworld@gmail.com>
2024-08-12 17:00:30 +0530

    Using Map function on React

--- amazon-seller/src/index.js
+++ amazon-seller/src/index.js
@@ -2,43 +2,28 @@ import React from 'react'
 import ReactDOM from "react-dom/client"
 import './index.css' ;
 
-const firstBook ={author:'Robert.T Kiyosaki' ,
+const books = [{author:'Robert.T Kiyosaki' ,
   bookTitle:'Rich Dad and Poor Dad' ,
   img:'https://images-eu.ssl-images-amazon.com/images/I/81BE7eeKzAL._AC_UL900_SR900,600_.jpg'
-}
-
-const secondBook ={
+}, {
   author :'The Housemaid', 
   bookTitle:'Freida McFadden', 
   img: 'https://images-na.ssl-images-amazon.com/images/I/81AHTyq2wVL._AC_UL900_SR900,600_.jpg' 
-}
-
-const thirdBook ={
+}, {
   author:'James Clear', 
   bookTitle:'Atomic Habits', 
   img:'https://images-na.ssl-images-amazon.com/images/I/81YkqyaFVEL._AC_UL900_SR900,600_.jpg'
-}
+} ]
 
+const names = ['immanuel', 'Rajkumar', 'junior']
+
+const newNames = names.map((names) => {
+  return <h1>{names}</h1>
+})
 
 const Booklist = () => {
   return (<section className='bookList'>
-    <Book 
-    author = {firstBook.author} 
-    title = {firstBook.bookTitle} 
-    img = {firstBook.img} >
-      <p>Solo Leveling is a web novel and webtoon series by Chu-Gong that follows the journey of Sung Jin-Woo, a hunter who gains strength independently. The series explores themes of independence, personal growth, and perseverance.</p>
-      <button>Click Me</button>
-      </Book>
-
-    <Book 
-    author = {secondBook.author} 
-    title = {secondBook.bookTitle} 
-    img = {secondBook.img}/>
-
-    <Book 
-    author = {thirdBook.author} 
-    title = {thirdBook.bookTitle} 
-    img = {thirdBook.img}/>
+   {newNames}
 
     </section>
   )
