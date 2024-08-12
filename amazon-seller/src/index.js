d6444eb31b07de3307b7eaf973958347d1819c47
MyCoding-Hub <breacher.darkworld@gmail.com>
2024-08-12 08:03:55 +0530

    store value in variables and retrive inside component

--- amazon-seller/src/index.js
+++ amazon-seller/src/index.js
@@ -2,6 +2,10 @@ import React from 'react'
 import ReactDOM from "react-dom/client"
 import './index.css' ;
 
+const author = 'Robert.T Kiyosaki'
+const book = 'Rich Dad and Poor Dad'
+const img = 'https://images-eu.ssl-images-amazon.com/images/I/81BE7eeKzAL._AC_UL900_SR900,600_.jpg'
+
 const Booklist = () => {
   return (<section className='bookList'>
     <Book/>
@@ -12,22 +16,17 @@ const Booklist = () => {
 }
 
 const Book = () => {
+
+const styleHeading = {color:'#617d98', fontSize:'0.75rem', marginTop:'0.5rem'}
     return(
         <article className='book'>
-        <Image/>
-        <Title/>
-        <Author/>
+          <img src={img} alt={book}/>
+          <h4>{book }</h4>
+          <h4 style={styleHeading}> {author}</h4>  
         </article>
     )
 }
-
-
-const Image =() => <img src='https://images-eu.ssl-images-amazon.com/images/I/81BE7eeKzAL._AC_UL900_SR900,600_.jpg' alt='Homemaid-secret'/>
-
-const Title = () => <h4> Rich Dad and Poor Dad</h4>
-
-const Author = () => <h4 style={{color:'#617d98', fontSize:'0.75rem', marginTop:'0.5rem'}}> Robert.T Kiyosaki</h4> 
-
+ 
 const root = ReactDOM.createRoot(document.getElementById("root"))
 
 root.render(<Booklist/>)
