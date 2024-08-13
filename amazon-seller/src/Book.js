fb4c06029ac5896f5bf4a69a47b273fb6dd3ce61
MyCoding-Hub <breacher.darkworld@gmail.com>
2024-08-13 19:29:12 +0530

    create span tag to display indexNumber

--- amazon-seller/src/Book.js
+++ amazon-seller/src/Book.js
@@ -1,6 +1,7 @@
 const Book = (props) => {
+    console.log(props)
 
-    const {author, title, img} = props
+    const {author, title, img, number} = props
   
     const styleHeading = {color:'#617d98', 
       fontSize:'0.75rem', 
@@ -8,6 +9,7 @@ const Book = (props) => {
   
     return(
       <article className='book'>
+        <span className="indexNumber">{`# ${number +1}`}</span>
         <img src={img} alt={title}/>
         <h4>{title}</h4>
         <button>Display Title</button>
