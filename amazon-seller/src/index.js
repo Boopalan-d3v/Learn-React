c4fc2ca6be52af2457a333c048166e4f50a51d6f
MyCoding-Hub <breacher.darkworld@gmail.com>
2024-08-13 17:25:58 +0530

    remove Book component and books objects from index.js

--- amazon-seller/src/index.js
+++ amazon-seller/src/index.js
@@ -1,58 +1,17 @@
 import React from 'react'
 import ReactDOM from "react-dom/client"
+import {books} from './books'
+import Book from './Book';
 import './index.css' ;
 
-const books = [{
-  id:1,
-  author:'Robert.T Kiyosaki' ,
-  title:'Rich Dad and Poor Dad' ,
-  img:'https://images-eu.ssl-images-amazon.com/images/I/81BE7eeKzAL._AC_UL900_SR900,600_.jpg'
-}, {
-  id:2,
-  author :'The Housemaid', 
-  title:'Freida McFadden', 
-  img: 'https://images-na.ssl-images-amazon.com/images/I/81AHTyq2wVL._AC_UL900_SR900,600_.jpg' 
-}, {
-  id:3,
-  author:'James Clear', 
-  title:'Atomic Habits', 
-  img:'https://images-na.ssl-images-amazon.com/images/I/81YkqyaFVEL._AC_UL900_SR900,600_.jpg'
-} ]
 
 const Booklist = () => {
-
-  const getBook = (id) => {
-    const book = books.find((data) => {return data.id === id})
-    console.log(book)
-  }
 
   return(
   <section className='bookList'>
     {books.map((books) => { 
-      return(<Book {...books} key= {books.id} getBook = {getBook}/> ) })}
+      return(<Book {...books} key= {books.id} /> ) })}
   </section>)}
-
-const Book = (props) => {
-
-  const {author, title, img, getBook, id} = props
-
-  const getSingleBook = () => {
-    getBook(id)
-  }
-
-  const styleHeading = {color:'#617d98', 
-    fontSize:'0.75rem', 
-    marginTop:'0.5rem'}
-
-  return(
-    <article className='book'>
-      <img src={img} alt={title}/>
-      <h4>{title}</h4>
-      <button onClick={getSingleBook}>Display Title</button>
-      <h4 style={styleHeading}> {author}</h4>  
-    </article>
-  )
-}
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
