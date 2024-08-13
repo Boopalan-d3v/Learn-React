43a870eb5c2c8bfcef2c59b7841be453a856c498
MyCoding-Hub <breacher.darkworld@gmail.com>
2024-08-13 17:27:50 +0530

    create books.js and store array of objects

---
+++ amazon-seller/src/books.js
@@ -0,0 +1,17 @@
+ export const books = [{
+    id:1,
+    author:'Robert.T Kiyosaki' ,
+    title:'Rich Dad and Poor Dad' ,
+    img:'https://images-eu.ssl-images-amazon.com/images/I/81BE7eeKzAL._AC_UL900_SR900,600_.jpg'
+  }, {
+    id:2,
+    author :'The Housemaid', 
+    title:'Freida McFadden', 
+    img: 'https://images-na.ssl-images-amazon.com/images/I/81AHTyq2wVL._AC_UL900_SR900,600_.jpg' 
+  }, {
+    id:3,
+    author:'James Clear', 
+    title:'Atomic Habits', 
+    img:'https://images-na.ssl-images-amazon.com/images/I/81YkqyaFVEL._AC_UL900_SR900,600_.jpg'
+  } ]
+  
