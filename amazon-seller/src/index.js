b36da9d34472b386dec16ff50b5b368efa99b96b
MyCoding-Hub <breacher.darkworld@gmail.com>
2024-08-13 19:28:10 +0530

    Add Title and Index number in bookList component

--- amazon-seller/src/index.js
+++ amazon-seller/src/index.js
@@ -8,10 +8,12 @@ import './index.css' ;
 const Booklist = () => {
 
   return(
+    <><h2>Best selling Books</h2>
   <section className='bookList'>
-    {books.map((books) => { 
-      return(<Book {...books} key= {books.id} /> ) })}
-  </section>)}
+    {books.map((books, index) => { 
+      return(<Book {...books} number = {index} /> ) }
+      )}
+  </section></>)}
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
