24d94f6145173f52e115eb308191e1454def2638
MyCoding-Hub <breacher.darkworld@gmail.com>
2024-08-13 17:54:50 +0530

    add images from local Image folder

--- amazon-seller/src/books.js
+++ amazon-seller/src/books.js
@@ -1,17 +1,27 @@
- export const books = [{
+import img2 from './Images/Homemaid-secret.jpg'
+import img3 from './Images/Atomic-Habits.jpg'
+import img4 from './Images/solo-Leveling.jpg'
+
+export const books = [{
     id:1,
     author:'Robert.T Kiyosaki' ,
     title:'Rich Dad and Poor Dad' ,
-    img:'https://images-eu.ssl-images-amazon.com/images/I/81BE7eeKzAL._AC_UL900_SR900,600_.jpg'
+    img:'https://images-eu.ssl-images-amazon.com/images/I/81BE7eeKzAL.,600_.jpg'
   }, {
     id:2,
     author :'The Housemaid', 
     title:'Freida McFadden', 
-    img: 'https://images-na.ssl-images-amazon.com/images/I/81AHTyq2wVL._AC_UL900_SR900,600_.jpg' 
+    img: img2 
   }, {
     id:3,
     author:'James Clear', 
     title:'Atomic Habits', 
-    img:'https://images-na.ssl-images-amazon.com/images/I/81YkqyaFVEL._AC_UL900_SR900,600_.jpg'
+    img: img3
+  },{
+    id:4,
+    author:'Hye Young Im', 
+    title:'Solo Leveling, Vol. 1 (comic) (Volume 1)', 
+    img: img4
+    
   } ]
   
