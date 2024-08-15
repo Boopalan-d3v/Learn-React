621818542b72f6fbb4ab37ddcda7261d07d02372
MyCoding-Hub <breacher.darkworld@gmail.com>
2024-08-15 19:53:17 +0530

    import details from data.js

--- backroads-app/src/components/Services.js
+++ backroads-app/src/components/Services.js
@@ -1,45 +1,28 @@
 import React from 'react'
 import { Title } from './Title'
+import { Service} from '../data'
 
 export const Services = () => {
   return (
     <section className="section services" id="services">
     <Title title='our' subTitle='services'/>
-      <div className="section-center services-center">
+    <div className="section-center services-center">
+    {
+      Service.map((data) => {
+        const {icon, title, text } = data
+        return(
         <article className="service">
-          <span className="service-icon"><i className="fas fa-wallet fa-fw"></i></span>
+          <span className="service-icon"><i className={icon}></i></span>
           <div className="service-info">
-            <h4 className="service-title">saving money</h4>
+            <h4 className="service-title">{title}</h4>
             <p className="service-text">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Asperiores, officia.
+              {text}
             </p>
           </div>
-        </article>
-
-        <article className="service">
-          <span className="service-icon"><i className="fas fa-tree fa-fw"></i></span>
-          <div className="service-info">
-            <h4 className="service-title">endless hiking</h4>
-            <p className="service-text">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Asperiores, officia.
-            </p>
-          </div>
-        </article>
-
-        <article className="service">
-          <span className="service-icon"><i className="fas fa-socks fa-fw"></i></span>
-          <div className="service-info">
-            <h4 className="service-title">amazing comfort</h4>
-            <p className="service-text">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Asperiores, officia.
-            </p>
-          </div>
-        </article>
-      </div>
+        </article>)
+      })
+    }
+    </div>
     </section>
-
   )
 }
