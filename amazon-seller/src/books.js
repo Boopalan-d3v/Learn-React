d637171123ccbe4580c02b6972535eecc1b11f8a
MyCoding-Hub <breacher.darkworld@gmail.com>
2024-08-15 10:40:35 +0530

    Add Description to all books

--- amazon-seller/src/books.js
+++ amazon-seller/src/books.js
@@ -4,24 +4,42 @@ import img4 from './Images/solo-Leveling.jpg'
 
 export const books = [{
     id:1,
-    author:'Robert.T Kiyosaki' ,
-    title:'Rich Dad and Poor Dad' ,
-    img:'https://images-eu.ssl-images-amazon.com/images/I/81BE7eeKzAL.,600_.jpg'
+    author:'Morgan Housel', 
+    title:'The Psychology of Money', 
+    img: 'https://images-eu.ssl-images-amazon.com/images/I/71XEsXS5RlL._AC_UL900_SR900,600_.jpg',
+    Description:'Timeless lessons on wealth, greed, and happiness doing well with money isn?t necessarily about what you know. It?s about how you behave. And behavior is hard to teach, even to really smart people. How to manage money, invest it, and make business decisions are typically considered to involve a lot of mathematical calculations, where data and formulae tell us exactly what to do.'
+
   }, {
     id:2,
     author :'The Housemaid', 
     title:'Freida McFadden', 
-    img: img2 
+    img: img2,
+    Description:'The Housemaid is a gripping novel that delves into the complex relationships and power dynamics within a wealthy household. When a young woman named Anne takes a job as a housemaid, she uncovers dark secrets and must navigate the dangerous world of the upper class. In this fun and quick-paced psychological thriller, Millie wonders what type of people she really working for...'
   }, {
     id:3,
     author:'James Clear', 
     title:'Atomic Habits', 
-    img: img3
+    img: img3,
+    Description:'People think that when you want to change your life, you need to think big. But world-renowned habits expert James Clear has discovered another way. He knows that real change comes from the compound effect of hundreds of small decisions: doing two push-ups a day, waking up five minutes early, or holding a single short phone call.'
   },{
     id:4,
     author:'Hye Young Im', 
     title:'Solo Leveling, Vol. 1 (comic) (Volume 1)', 
-    img: img4
+    img: img4,
+    Description:'A notoriously weak hunter named Sung Jinwoo finds himself in a seemingly endless struggle for survival. One day, after narrowly surviving an overwhelmingly powerful double dungeon that nearly wipes out his entire party, a mysterious program called the System chooses him as its sole player and in turn, gives him the extremely rare ability to level up in strength, possibly beyond any known limits. Jinwoo sets out on a journey as he fights against all kinds of enemies, both man and monster, to discover the secrets of the dungeons and the true source of his powers.'
     
+  },{
+    id:5,
+    author:'Robert.T Kiyosaki' ,
+    title:'Rich Dad and Poor Dad' ,
+    img:'https://images-eu.ssl-images-amazon.com/images/I/81BE7eeKzAL.,600_.jpg',
+    Description:'25 years have passed since Rich Dad Poor Dad was first published, readers will find that very little in the book itself has changed — and for good reason. While so much in our world is changing a high speed, the lessons about money and the principles of Rich Dad Poor Dad haven’t changed. Today, as money continues to play a key role in our daily lives, the messages in Robert Kiyosaki’s international bestseller are more timely and more important than ever.'
+    
+  },{
+    id:6,
+    author:'Anita Krishan', 
+    title:'Ghosts of The Silent Hills: Stories based on true hauntings', 
+    img:'https://m.media-amazon.com/images/I/71NUqTiGrOL._AC_UY327_FMwebp_QL65_.jpg',
+    Description : 'The dead do not rest till they get what they want. You have arrived in the hills. In here, you are surrounded by dense, menacing forests, enveloped in a deadly silence... You never know what lurks here in the Cold, dark night. Do not walk alone after sunset in the hills. A beautiful woman in white haunts the Lonely pathways, looking to enchant and ensnare men... All the people who died in accidents here... They say you hear their screams at night. And the deserted lodges sitting amidst lush greenery and calm streams...'
   } ]
   
