0db2fd8231488642e3841ed6ccc1388fe2679b52
MyCoding-Hub <breacher.darkworld@gmail.com>
2024-08-15 19:39:05 +0530

    Add Tour details in a array

--- backroads-app/src/data.js
+++ backroads-app/src/data.js
@@ -9,3 +9,73 @@ export const SocialLinks =[
     {id:2, href:'https://www.twitter.com', className:"fab fa-twitter"},
     {id:3, href:'https://www.squarespace.com', className:"fab fa-squarespace"}
 ]
+
+export const Service =[
+    {id:1, 
+    icon:'fas fa-wallet fa-fw', 
+    title:'saving money', 
+    text:'Lorem ipsum dolor sit amet consectetur adipisicing elit Asperiores, officia.' },
+    {id:2, 
+    icon:'fas fa-tree fa-fw', 
+    title:'endless hiking', 
+    text:'Lorem ipsum dolor sit amet consectetur adipisicing elit Asperiores, officia.'
+    },
+    {id:3, 
+    icon:'fas fa-socks fa-fw', 
+    title:'amazing comfort', 
+    text:'Lorem ipsum dolor sit amet consectetur adipisicing elit Asperiores, officia.'}]
+
+export const Tour =[
+    {id:1,
+    img:'./images/tour-1.jpeg',
+    data:'august 26th, 2020',
+    text:'Tibet Adventure',
+    info:'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.',
+    place:'china',
+    days:'6 days',
+    price:'from $2100'
+    },
+    {id:2,
+    img:'./images/tour-2.jpeg',
+    data:'october 1th, 2020',
+    text:'best of java',
+    info:'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.',
+    place:'indonesia',
+    days:'11 days',
+    price:'from $1400'
+    },
+    {id:3,
+    img:'./images/tour-3.jpeg',
+    data:'september 15th, 2020',
+    text:'explore hong kong',
+    info:'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.',
+    place:'hong kong',
+    days:'8 days',
+    price:'from $5000'
+    },
+    {id:4,
+    img:'./images/tour-4.jpeg',
+    data:'december 5th, 2019',
+    text:'kenya highlights',
+    info:'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.',
+    place:'kenya',
+    days:'20 days',
+    price:'from $3300'
+    } /*
+    {id:5,
+    img:'./images/tour-5.jpeg',
+    data:'december 5th, 2019',
+    text:'kenya highlights',
+    info:'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.',
+    place:'kenya',
+    days:'20 days',
+    price:'from $3300'
+    },
+    {id:6,
+    img:'./images/tour-6.jpeg',
+    data:'december 5th, 2019',
+    text:'kenya highlights',
+    info:'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.',
+    place:'kenya',
+    days:'20 days',
+    price:'from $3300'}*/]
