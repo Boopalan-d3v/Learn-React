610ebcfaccb37e3074379a47a55c210f038af04a
MyCoding-Hub <breacher.darkworld@gmail.com>
2024-08-17 18:07:48 +0530

    change values using useState

--- advanced-react/src/tutorial/01-useState/starter/04-useState-object.jsx
+++ advanced-react/src/tutorial/01-useState/starter/04-useState-object.jsx
@@ -1,7 +1,28 @@
-import { useState } from 'react';
+import React from "react";
+import { peopleHobby } from "../../../data";
+import { useState } from "react";
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
+
+  const [name, setName] = useState('Ashi')
+  const [age, setAge] = useState(27)
+  const [hobby, setHobby] = useState('Dancing')
+    
+  const showDetails = () =>{
+    setName('Lallu')
+    setAge(23)
+    setHobby('Gardening')
+  }
+
+  return( 
+    <>
+      <h2>useState object example</h2>
+      <h2>{name}</h2>
+      <h2>{age}</h2>
+      <h2>{`Enjoy's ${hobby}`}</h2>
+      <button onClick={showDetails}>Show lallu</button>
+    </>);
+
 };
 
 export default UseStateObject;
