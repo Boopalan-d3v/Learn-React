8815779f9fd172830ef741cee6bba503f869b779
MyCoding-Hub <breacher.darkworld@gmail.com>
2024-08-17 17:19:17 +0530

    Delete single item and multiple item

--- advanced-react/src/tutorial/01-useState/starter/03-useState-array.jsx
+++ advanced-react/src/tutorial/01-useState/starter/03-useState-array.jsx
@@ -1,5 +1,33 @@
+import { data } from "../../../data";
+import React from 'react'
+
 const UseStateArray = () => {
-  return <h2>useState array example</h2>;
+  const [people, setPeople] = React.useState(data)
+
+  const clearItem = (id) => {
+    const newPeople = people.filter((people) => people.id !== id)
+    setPeople(newPeople)
+  }
+  const clearAllItems = () => {
+    setPeople([])
+  }
+
+
+  return(
+    <>
+      {people.map((person) => {
+        const {id, name} = person
+        console.log(person);
+        
+        return(<>
+          <h4 key={id}>{name}</h4>
+          <button className="btn" onClick={() => clearItem(id)}>Clear Item</button>
+          </>)
+      })}
+      
+     
+     <button className='btn' onClick={clearAllItems} style={{marginTop:'2rem'}}>Clear all Items</button>
+    </>);
 };
 
 export default UseStateArray;
