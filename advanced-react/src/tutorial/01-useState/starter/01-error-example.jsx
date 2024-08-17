ea83886771cd5db83a2e161176f491bc9564f137
MyCoding-Hub <breacher.darkworld@gmail.com>
2024-08-17 07:51:05 +0530

    create variable to return increment value on console

--- advanced-react/src/tutorial/01-useState/starter/01-error-example.jsx
+++ advanced-react/src/tutorial/01-useState/starter/01-error-example.jsx
@@ -1,5 +1,17 @@
 const ErrorExample = () => {
-  return <h2>useState error example</h2>;
+  let count = 0
+  
+  const handleClick = () => {
+    count +=1 
+    console.log(count)
+ 
+  }
+
+  return (
+    <>
+      <h2 id='ivalue'></h2>
+      <button onClick={handleClick} className='btn'>Increment</button>
+    </>);
 };
 
 export default ErrorExample;
