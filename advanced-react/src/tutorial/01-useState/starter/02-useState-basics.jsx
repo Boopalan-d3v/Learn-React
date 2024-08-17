c500658c806c585c0507946bd005b745e6716173
MyCoding-Hub <breacher.darkworld@gmail.com>
2024-08-17 08:47:26 +0530

    learn about useState

--- advanced-react/src/tutorial/01-useState/starter/02-useState-basics.jsx
+++ advanced-react/src/tutorial/01-useState/starter/02-useState-basics.jsx
@@ -1,5 +1,22 @@
+import {useState} from 'react'
+
 const UseStateBasics = () => {
-  return <h2>useState basics</h2>;
+  //console.log(useState('bob'))
+  //const value = useState('hello')[0]
+  //const value = useState('world')[1]
+  // console.log(value)
+  // console.log(func)
+
+  let [count, setCount] = useState(0)
+
+  let handleClick = () => {
+    setCount(count = count + 1)
+  }
+
+  return (<>
+    <h4>You Clicked {count} times</h4>
+    <button className='btn' onClick={handleClick}>Increament</button>
+    </>);
 };
 
 export default UseStateBasics;
