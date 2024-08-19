a89f40da39c940452d40b8bc260fe1a980c6ab34
MyCoding-Hub <breacher.darkworld@gmail.com>
2024-08-19 11:48:14 +0530

    by using useState hook Increase the value by one

--- advanced-react/src/tutorial/01-useState/starter/05-useState-gotcha.jsx
+++ advanced-react/src/tutorial/01-useState/starter/05-useState-gotcha.jsx
@@ -1,5 +1,21 @@
+import { useState} from 'react'
+
 const UseStateGotcha = () => {
-  return <h2>useState "gotcha"</h2>;
+    const [value, setValue] = useState(0)
+
+    const handleClick = () => {
+      setValue((currentValue) => {
+        const newState = currentValue + 1
+        return newState
+      })
+      console.log(value)
+    }
+
+    return(<> 
+    <h2>useState "gotcha"</h2>
+    <h2>{value}</h2>
+    <button onClick={handleClick} className='btn'>Increment</button>
+  </>)
 };
 
 export default UseStateGotcha;
