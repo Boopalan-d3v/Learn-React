26bb23fa48924d565a9aec28a143f6c6403c4ead
MyCoding-Hub <breacher.darkworld@gmail.com>
2024-08-21 18:06:40 +0530

    fetch error gotcha completed

--- advanced-react/src/tutorial/03-conditional-rendering/starter/02-multiple-returns-fetch-data.jsx
+++ advanced-react/src/tutorial/03-conditional-rendering/starter/02-multiple-returns-fetch-data.jsx
@@ -4,28 +4,34 @@ const url = 'https://api.github.com/users/QuincyLarson';
 const MultipleReturnsFetchData = () => {
   const [isLoading, setIsLoading] = useState(true)
   const [isError, setIsError] = useState(false)
-  const [user, setUser] = useState([])
+  const [user, setUser] = useState(null)
 
   useEffect(()=>{
     const data = async() => {
       try {
         const response = await fetch(url)
+        if(!response.ok){
+          setIsError(true)
+          setIsLoading(false)
+          return;
+        }
         const users = await response.json()
         setUser(users)
       } catch (error) {
         setIsError(true)
         console.log(error); 
       }
+      setIsLoading(false)
     }
     data()
   }, [])
 
-//if(isLoading){
-////return <h1>Loading ...</h1>
-//}
-  if(isError){
-    return <h1>There was an error !</h1>
+  if(isLoading){
+    return <h1>Loading ...</h1>
   }
+if(isError){
+  return <h1>There was an error !</h1>
+}
 
   const {avatar_url, name, company, bio} = user
   return(<>
