bfc4661fe935bd7f9a3191bc83d37820ecd099fd
MyCoding-Hub <breacher.darkworld@gmail.com>
2024-08-21 17:40:36 +0530

    complete the fetch data task but confused

--- advanced-react/src/tutorial/03-conditional-rendering/starter/02-multiple-returns-fetch-data.jsx
+++ advanced-react/src/tutorial/03-conditional-rendering/starter/02-multiple-returns-fetch-data.jsx
@@ -2,6 +2,40 @@ import { useEffect, useState } from 'react';
 const url = 'https://api.github.com/users/QuincyLarson';
 
 const MultipleReturnsFetchData = () => {
-  return <h2>Fetch Data </h2>;
+  const [isLoading, setIsLoading] = useState(true)
+  const [isError, setIsError] = useState(false)
+  const [user, setUser] = useState([])
+
+  useEffect(()=>{
+    const data = async() => {
+      try {
+        const response = await fetch(url)
+        const users = await response.json()
+        setUser(users)
+      } catch (error) {
+        setIsError(true)
+        console.log(error); 
+      }
+    }
+    data()
+  }, [])
+
+//if(isLoading){
+////return <h1>Loading ...</h1>
+//}
+  if(isError){
+    return <h1>There was an error !</h1>
+  }
+
+  const {avatar_url, name, company, bio} = user
+  return(<>
+    <h2>Fetch Data </h2>
+    <section>
+      <img style={{width:'150px', borderRadius:'25px'}} src={avatar_url} alt={name} />
+      <h2>{name}</h2>
+      <h3>Works At {company}</h3>
+      <h4>{bio}</h4>    
+    </section></>);
 };
+
 export default MultipleReturnsFetchData;
