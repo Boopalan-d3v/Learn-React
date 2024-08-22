3ea3abc49e5ed195e2427fa572717beeb005a37c
MyCoding-Hub <breacher.darkworld@gmail.com>
2024-08-22 08:46:22 +0530

    learn about short circuit with example

--- advanced-react/src/tutorial/03-conditional-rendering/starter/05-short-circuit-examples.jsx
+++ advanced-react/src/tutorial/03-conditional-rendering/starter/05-short-circuit-examples.jsx
@@ -8,7 +8,23 @@ const ShortCircuitExamples = () => {
   const [user, setUser] = useState({ name: 'john' });
   const [isEditing, setIsEditing] = useState(false);
 
-  return <h2>short circuit - examples</h2>;
+  return <>
+    <h2>short circuit - examples</h2>
+    <h2>{text || user.name}</h2>
+    {/*!text && (<div>
+      <h2>whatever</h2>
+      <h2>{name}</h2>
+    </div>) */}
+    {user && <SomeComponent name={user.name} />}
+    </>;
 };
 
+const SomeComponent = ({name}) => {
+  return <>
+    <h3>Returned value</h3>
+    <h3>{name}</h3></>
+}
+
+
 export default ShortCircuitExamples;
+
