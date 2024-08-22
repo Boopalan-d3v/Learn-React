ae5e9776d7adb0c8910a7b909e652a227182b699
MyCoding-Hub <breacher.darkworld@gmail.com>
2024-08-22 12:12:38 +0530

    complete user challenge

--- advanced-react/src/tutorial/03-conditional-rendering/starter/07-user-challenge.jsx
+++ advanced-react/src/tutorial/03-conditional-rendering/starter/07-user-challenge.jsx
@@ -1,5 +1,23 @@
+import { useState} from 'react'
+
 const UserChallenge = () => {
-  return <h2>user challenge</h2>;
+
+  const [user, setUser] = useState (null)
+  
+  const logIn = () => {
+    setUser({name:'ashi'})
+  }
+
+  const logOut = () => {
+    setUser(null)
+  }
+
+  return<>
+    <h2>user challenge</h2>
+    {user? (<div><h1>hello there, {user.name}</h1>
+    <button className='btn' onClick={logOut}> Log Out</button></div>) : (<div><h1>Please Log In</h1>
+    <button className='btn' onClick={logIn}>Log in</button></div>)}
+    </>;
 };
 
 export default UserChallenge;
