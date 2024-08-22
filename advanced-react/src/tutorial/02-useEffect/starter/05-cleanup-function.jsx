5dc4bb5e59296c008ab8d12f7e5dd94623a8150e
MyCoding-Hub <breacher.darkworld@gmail.com>
2024-08-22 13:47:07 +0530

    complete cleanup challenge

--- advanced-react/src/tutorial/02-useEffect/starter/05-cleanup-function.jsx
+++ advanced-react/src/tutorial/02-useEffect/starter/05-cleanup-function.jsx
@@ -1,5 +1,27 @@
+import { useEffect, useState } from "react";
 const CleanupFunction = () => {
-  return <h2>cleanup function</h2>;
+
+  const [toggle, setToggle] = useState(false)
+  console.log('hey, There !');
+  
+  return <div>
+    <h2>cleanup function</h2>
+    <button className="btn" onClick={() => {setToggle(!toggle)}}>Toggle</button>
+    {toggle && <SecondComponent />}
+  </div>;
 };
 
+const SecondComponent = () => {
+  useEffect(() => {
+    const intId = setInterval(() => {
+      //console.log('Wooofh !');
+    } , 1000)
+    return() => {
+      clearInterval(intId)
+      console.log('cleanup')
+    }   
+  }, [])
+
+  return <><h3>hello, Techie</h3></>
+}
 export default CleanupFunction;
