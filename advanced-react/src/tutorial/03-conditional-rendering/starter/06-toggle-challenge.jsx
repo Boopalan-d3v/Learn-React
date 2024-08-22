667d60211a70aa02f01b38e2f8e89fb237803602
MyCoding-Hub <breacher.darkworld@gmail.com>
2024-08-22 11:40:39 +0530

    complete toggle challenge

--- advanced-react/src/tutorial/03-conditional-rendering/starter/06-toggle-challenge.jsx
+++ advanced-react/src/tutorial/03-conditional-rendering/starter/06-toggle-challenge.jsx
@@ -1,5 +1,26 @@
+import {useState} from 'react'
+
 const ToggleChallenge = () => {
-  return <h2>toggle challenge</h2>;
+
+const [showAlert , setShowAlert] = useState(false)
+
+/*const handleClick = () => {
+  if(showAlert){
+    setShowAlert(false)
+    return;
+  }
+  setShowAlert(true)
+} */
+
+  return<>
+   <h2>toggle challenge</h2>
+   <button className='btn' onClick={()=>{setShowAlert(!showAlert)}}>Hello</button>
+   {showAlert && <Alert />}
+   </>;
 };
+
+const Alert = () => {
+  return <div className='alert alert-danger'>Hello world</div>
+}
 
 export default ToggleChallenge;
