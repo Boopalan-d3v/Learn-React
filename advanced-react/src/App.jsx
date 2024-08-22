9e70828590f22a43bceda9e90ffd63bca877bf20
MyCoding-Hub <breacher.darkworld@gmail.com>
2024-08-22 18:46:13 +0530

    import Example component from Example folder

--- advanced-react/src/App.jsx
+++ advanced-react/src/App.jsx
@@ -1,10 +1,10 @@
-import { About, Home } from '../src/tutorial/04-project-structure/starter/Pages'
+import Example from "./tutorial/04-project-structure/starter/Example";
+
 
 function App() {
   return (
     <div className='container'>
-      <Home/>
-      <About />
+      <Example/>
     </div>
   );
 }
