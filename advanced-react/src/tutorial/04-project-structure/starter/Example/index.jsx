0f5bf317013a6ac84be95773d044fbd45caf8c84
MyCoding-Hub <breacher.darkworld@gmail.com>
2024-08-22 18:48:20 +0530

    import first and component inside the Example component

---
+++ advanced-react/src/tutorial/04-project-structure/starter/Example/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import FirstComponent from './firstComponent'
+import SecondComponent from './secondComponent'
+
+function Example() {
+  return (
+    <div>
+        <FirstComponent/>
+        <SecondComponent/>
+    </div>
+  )
+}
+
+export default Example
