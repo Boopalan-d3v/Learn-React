11559f7d75622b75b4237538800553c2ccaac7a6
MyCoding-Hub <breacher.darkworld@gmail.com>
2024-09-04 16:34:50 +0530

    create Navlink component

---
+++ advanced-react/src/tutorial/09-context-api/starter/NavLinks.jsx
@@ -0,0 +1,17 @@
+import { UserContainer } from "./UserContainer";
+
+export function NavLinks() {
+  return (
+    <div className="nav-container">
+      <ul className="nav-links">
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#">About</a>
+        </li>
+      </ul>
+      <UserContainer />
+    </div>
+  )
+}
