e457ca1213d0cd867ba36d8a1b988d8493dc0d23
MyCoding-Hub <breacher.darkworld@gmail.com>
2024-09-05 19:15:47 +0530

    practice useReducer hook with SwithCase

--- advanced-react/src/tutorial/10-useReducer/starter/01-useReducer.jsx
+++ advanced-react/src/tutorial/10-useReducer/starter/01-useReducer.jsx
@@ -1,39 +1,49 @@
-import React, { useState , useReducer} from 'react';
-import { data } from '../../../data';
+import React, { /*useState , */ useReducer} from 'react';
+import { data, people } from '../../../data';
+
+const Actions = {clearALL:'CLEAR_ALL', restore:'RESTORE', removeItem:'REMOVE_ITEM'}
+
 const ReducerBasics = () => {
+  const defaultData = {
+    people:data,
+  }
 
   const reducer = (state, action) => {
     switch (action.type) {
-      case "ClearAll":
-        
-        break;
-    
-      default:
-        break;
+      case Actions.restore:
+        return{...state, people:data}
+      case Actions.removeItem:
+        let newData = people.filter((person) => person.id !== state.id)
+        return{...state, people:newData}
+      case Actions.clearALL:
+        return{...state, people:[]}
     }
 
   }
 
-   const [state, dispatch] = useReducer(reducer,data)
+   const [state, dispatch] = useReducer(reducer,defaultData)
 
 
   //const [people, setPeople] = useState(data);
 
   const removeItem = (id) => {
-    let newPeople = people.filter((person) => person.id !== id);
-    setPeople(newPeople);
+    dispatch({type: Actions.removeItem, id})
+    //let newPeople = people.filter((person) => person.id !== id);
+    //setPeople(newPeople);
   };
 
   const clearAll = () => {
+    dispatch({type:Actions.clearALL})
     //setPeople([])
   }
 
   const restoreItem = () => {
+    dispatch({type: Actions.restore})
     //setPeople (data)
   }
   return (
     <div>
-      {people.map((person) => {
+      {state.people.map((person) => {
         const { id, name } = person;
         return (
           <div key={id} className='item'>
@@ -42,7 +52,7 @@ const ReducerBasics = () => {
           </div>
         );
       })}
-      {people.length ? (<><button className='btn' style={{ marginTop: '2rem' }} onClick={clearAll}>
+      {state.people.length ? (<><button className='btn' style={{ marginTop: '2rem' }} onClick={clearAll}>
         clear items
       </button></>) : (<><button className='btn' style={{ marginTop: '2rem' }} onClick={restoreItem}>
         Restore items
