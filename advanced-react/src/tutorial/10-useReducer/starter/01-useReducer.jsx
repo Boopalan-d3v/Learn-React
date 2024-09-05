ca8b0365c23111ccf389653ff1d72c7dc7bd715c
MyCoding-Hub <breacher.darkworld@gmail.com>
2024-09-05 11:34:10 +0530

    learn about useReducer hook

--- advanced-react/src/tutorial/10-useReducer/starter/01-useReducer.jsx
+++ advanced-react/src/tutorial/10-useReducer/starter/01-useReducer.jsx
@@ -1,15 +1,37 @@
-import React, { useState } from 'react';
+import React, { useState, useReducer } from 'react';
 import { data } from '../../../data';
+import {CLEAR_LIST , RESET_LIST, REMOVE_ITEM} from './actions'
+import reducerFunc from './reducer';
+
 const ReducerBasics = () => {
-  const [people, setPeople] = useState(data);
+  //const [people, setPeople] = useState(data);
+
+  const defaultState = {
+    people:data,
+    isLoading:false
+  }
+
+  const [state, dispatch] = useReducer(reducerFunc, defaultState)
 
   const removeItem = (id) => {
-    let newPeople = people.filter((person) => person.id !== id);
-    setPeople(newPeople);
+    dispatch({type: REMOVE_ITEM, payload : {id}})
+    //let newPeople = people.filter((person) => person.id !== id);
+    //setPeople(newPeople);
   };
+
+  const clearAll = () =>{
+    dispatch({type: CLEAR_LIST})
+    //setPeople([])
+  }
+
+  const restoreItems = () => {
+    //setPeople(data)
+    dispatch({type: RESET_LIST})
+  }
+  console.log(state);  
   return (
     <div>
-      {people.map((person) => {
+      {state.people.map((person) => {
         const { id, name } = person;
         return (
           <div key={id} className='item'>
@@ -18,9 +40,9 @@ const ReducerBasics = () => {
           </div>
         );
       })}
-      {people.length ? (<><button className='btn' style={{ marginTop: '2rem' }} onClick={() => setPeople([])}>
+      {state.people.length ? (<><button className='btn' style={{ marginTop: '2rem' }} onClick={clearAll}>
         clear items
-      </button></>) : (<><button className='btn' style={{ marginTop: '2rem' }} onClick={() => setPeople(data)}>
+      </button></>) : (<><button className='btn' style={{ marginTop: '2rem' }} onClick={restoreItems}>
         Restore items
       </button></>)}
       
