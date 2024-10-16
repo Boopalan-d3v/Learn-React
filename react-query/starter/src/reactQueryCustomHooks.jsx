b6b4f463a03ce7450bee1bc06fe050a64fff0fac
MyCoding-Hub <breacher.darkworld@gmail.com>
2024-10-16 16:22:43 +0530

    create custom hook for Edit & Delete task

--- react-query/starter/src/reactQueryCustomHooks.jsx
+++ react-query/starter/src/reactQueryCustomHooks.jsx
@@ -1,4 +1,4 @@
-import { useQuery } from "@tanstack/react-query"
+import { useQuery, useMutation, useQueryClient} from "@tanstack/react-query"
 import customFetch from "./utils"
 
 export const useFetchTasks = () => {
@@ -8,6 +8,37 @@ export const useFetchTasks = () => {
           const {data} = await customFetch.get('/')
           return data
         }
-      })
+    })
   return { isLoading, data, isError}    
+}
+
+export const useCreateTask = () => {
+
+}
+
+export const useEditTask = () => {
+    const queryClient = useQueryClient()
+    const {mutate:editTask} = useMutation({
+        mutationFn: ({taskId, isDone}) => {
+            return customFetch.patch(`/${taskId}`, {isDone})
+        },
+        onSuccess:() => {
+            queryClient.invalidateQueries({queryKey: ['tasks']})
+        },
+    })
+    return { editTask }
+}
+
+export const useDeleteTask = () => {
+    const queryClient = useQueryClient()
+    const {mutate:deleteTask, isLoading:deleteTaskLoading} = useMutation({
+        mutationFn:(taskId) => {
+          return customFetch.delete(`/${taskId}`)
+        },
+        onSuccess:() => {
+          toast.success("task deleted")
+          queryClient.invalidateQueries({queryKey:['tasks']})
+        }
+    })
+    return { deleteTask, deleteTaskLoading}
 }
